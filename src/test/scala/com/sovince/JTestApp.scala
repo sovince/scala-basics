9204ce4d12804ffb1611c68f44354d6617417a56
Vince <so_vince@outlook.com>
2019-06-10 09:41:11 +0800

    junit

---
+++ src/test/scala/com/sovince/JTestApp.scala
@@ -0,0 +1,19 @@
+package com.sovince
+
+import org.junit.Test
+
+/**
+  * Created by vince
+  * Email: so_vince@outlook.com
+  * Data: 2019/6/10
+  * Time: 09:37
+  * Description:
+  */
+class JTestApp{
+
+  @Test
+  def foo(): Unit ={
+    print(1)
+  }
+
+}
